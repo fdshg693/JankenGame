15489f0d6846c394eb9e753e378a7155f6a07d36
fdshg693 <xingwanmeixiong@gmail.com>
2025-05-25 14:35:47 +0900

    拡張メソッドを使って、ジャンケンの手の表示を日本語に

--- Models/JankenHand.cs
+++ Models/JankenHand.cs
@@ -1,9 +1,48 @@
-﻿namespace JankenGame.Models
+﻿using System.ComponentModel;
+using System.Reflection;
+
+
+namespace JankenGame.Models
 {
-	public enum JankenHand
-	{
-		Rock = 0, // グー
-		Paper = 1, // パー
-		Scissors = 2 // チョキ
+    public enum JankenHand
+    {
+        [Description("グー")]
+        Rock = 0,
+        [Description("パー")]
+        Paper = 1,
+        [Description("チョキ")]
+        Scissors = 2
+    }
+}
+
+/// <summary>
+/// enum 全般を拡張する静的クラス
+/// </summary>
+public static class EnumExtensions
+{
+    /// <summary>
+    /// enum の全ての値を取得する
+    /// </summary>
+    public static IEnumerable<T> GetValues<T>() where T : Enum
+        => Enum.GetValues(typeof(T)).Cast<T>();
+
+    /// <summary>
+    /// enum の名前 (識別子) を取得する。ToString() と同じ
+    /// </summary>
+    public static string GetName<T>(this T value) where T : Enum
+        => value.ToString();
+
+    /// <summary>
+    /// DescriptionAttribute を付けていればそれを、なければ ToString() を返す
+    /// </summary>
+    public static string GetDescription<T>(this T value) where T : Enum
+    {
+        var fi = value.GetType().GetField(value.ToString());
+        if (fi == null)
+            return value.ToString();
+        var attr = fi.GetCustomAttribute<DescriptionAttribute>();
+        return attr != null
+            ? attr.Description
+            : value.ToString();
     }
 }
